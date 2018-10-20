713c283efc31e698f13dab0a0d6f5ebcb2f2c1ba
Eduardo Gamarra <inoku_0099@hotmail.com>
2018-10-20 17:25:35 -0500

    init 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <p>Hola mundo
+            <p>Esto va en azul</p>
+          </p>
+          <div>
+            <p>
+            Esto va en verde
+          </p>
+          </div>
           <img src={logo} className="App-logo" alt="logo" />
+          
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          
           <a
             className="App-link"
             href="https://reactjs.org"
