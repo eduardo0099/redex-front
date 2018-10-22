2e793d674ea2a71bf9adc0a174a6597a2de092f6
Johann Morales <johann.morales@pucp.edu.pe>
2018-10-22 00:27:18 -0500

    update

--- src/components/layout/SidebarLayout.js
+++ src/components/layout/SidebarLayout.js
@@ -1,5 +1,9 @@
 import  React from 'react';
 import { Layout, Menu, Icon } from 'antd';
+import { Link } from "react-router-dom";
+import routes from '../../routes';
+import _ from 'lodash';
+
 const SubMenu = Menu.SubMenu;
 const { Header, Sider } = Layout;
 
@@ -18,29 +22,17 @@ export default class SidebarLayout extends React.Component {
           <Sider breakpoint="lg" collapsedWidth="80">
             <div className="logo"/>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
-              <Menu.Item key="3">Option 3</Menu.Item>
-              <Menu.Item key="4">Option 4</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigtion Two</span></span>}>
-              <Menu.Item key="5">Option 5</Menu.Item>
-              <Menu.Item key="6">Option 6</Menu.Item>
-              <SubMenu key="sub3" title="Submenu">
-                <Menu.Item key="7">Option 7</Menu.Item>
-                <Menu.Item key="8">Option 8</Menu.Item>
-              </SubMenu>
-            </SubMenu>
-            <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
-              <Menu.Item key="9">Option 9</Menu.Item>
-              <Menu.Item key="10">Option 10</Menu.Item>
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu>
+            { _.map(routes, (route, key) => {
+            const { component, path } = route;
+            return (
+              <Menu.Item key={key}>
+                <Link to={path}> { path } </Link>
+              </Menu.Item>
+            )
+          })}
             </Menu>
           </Sider>
-            <Component route={route}/>
+          <Component route={route}/>
         </Layout>
       </div>
    );
