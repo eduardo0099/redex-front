c6b1b0d5ecca605f7fb60ce49cbcc6f25e1d955d
Johann Morales <johann.morales@pucp.edu.pe>
2018-10-22 07:46:46 -0500

    uodate

--- src/views/oficinas/index.js
+++ src/views/oficinas/index.js
@@ -2,14 +2,40 @@ import React, { Component } from 'react';
 import { Row, Col, Layout, Button } from 'antd';
 import { TheContent, TheHeader } from '../../components/layout';
 import OficinasList from './OficinasList';
-import OficinasModal from './OficinasModal';
+import OficinasForm from './OficinasForm';
 
 export default class Oficinas extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-
+      modalVisible: false
     }
+  }
+
+  showModal = () => {
+    this.setState({ modalVisible: true });
+  }
+
+  handleCancel = () => {
+    this.setState({ modalVisible: false });
+  }
+
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log('Received values of form: ', values);
+      form.resetFields();
+      this.setState({ modalVisible: false });
+    });
+  }
+
+  saveFormRef = (formRef) => {
+    this.formRef = formRef;
   }
 
   render() {
@@ -20,13 +46,15 @@ export default class Oficinas extends React.Component {
               <h2> Oficinas </h2>
             </Col>
             <Col span={12} align="right">
-              <Button type="primary"> Nuevo </Button>
+              <Button type="primary" onClick={this.showModal}> Nuevo </Button>
             </Col>
           </TheHeader>
           <TheContent>
               <OficinasList/>
+              <OficinasForm visible={this.state.modalVisible} onCancel={this.handleCancel} onCreate={this.handleCreate} wrappedComponentRef={this.saveFormRef}/>
           </TheContent>
         </Layout>
     )
   }
 }
+
