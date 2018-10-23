98b56dc37d9effe28b14ebd40dfe8025a6020696
Johann Morales <johann.morales@pucp.edu.pe>
2018-10-23 14:40:37 -0500

    update

--- src/views/oficinas/OficinasList.js
+++ src/views/oficinas/OficinasList.js
@@ -1,81 +1,106 @@
 import React from 'react';
-import { Table, Divider, Tag } from 'antd';
+import { Table, Tag, Dropdown, Menu, Icon} from 'antd';
+import API from '../../Services/Api';
 
 const { Column, ColumnGroup } = Table;
 
-const oficinas = [
-    {codigo: 'PER', pais: {nombre: "Peru"}, capacidadActual: 0, capacidadMaxima: 200}
-]
-
-
-const data = [{
-    key: '1',
-    firstName: 'John',
-    lastName: 'Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  }, {
-    key: '2',
-    firstName: 'Jim',
-    lastName: 'Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  }, {
-    key: '3',
-    firstName: 'Joe',
-    lastName: 'Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  }];
-
 export default class OficinasList extends React.Component {
     
+  constructor(props){
+    super(props)
+    this.state = {
+      oficinas: []
+    }
+
+  }
+
+  componentDidMount() {
+    this.list();
+  }
+
+  list = () => {
+    API.get('oficinas')
+      .then(response => {
+        this.setState({...this.state, oficinas: response.data}, () => console.dir(this.state.oficinas));
+    })
+  }
+
+  activar = (record) => {
+    API.post(`oficinas/${record.id}/activar`)
+    .then(response => {
+      this.list();
+    })
+  }
+
+  desactivar = (record) => {
+    API.post(`oficinas/${record.id}/desactivar`)
+    .then(response => {
+      this.list();
+    })
+  }
     render(){
+      
+
         return (
-            <Table dataSource={data}>
+            <Table dataSource={this.state.oficinas} rowKey="id">
               <Column
-                title="First Name"
-                dataIndex="firstName"
-                key="firstName"
+                title="Código"
+                key="codigo"
+                render={record => (
+                  <div>
+                    <div>
+                  <b> { record.pais.nombre } </b>
+                  </div>
+                  <small> { record.codigo } </small>
+                  </div>
+                )}
               />
               <Column
-                title="Last Name"
-                dataIndex="lastName"
-                key="lastName"
+                title="Capacidad"
+                key="capacidad"
+                render={record => (
+                  <span> {record.capacidadActual}/{record.capacidadMaxima}</span>
+                )}
               />
             <Column
-              title="Age"
-              dataIndex="age"
-              key="age"
+              title="Estado"
+              dataIndex="estado"
+              key="estado"
+              render={estado => {
+                  switch(estado.name){
+                    case 'ACTIVO':
+                      return (<Tag color="green"> Activo </Tag>);
+                    case 'INACTIVO':
+                      return (<Tag color="red"> Inactivo </Tag>);
+                  }
+              }}
             />
             <Column
-              title="Address"
-              dataIndex="address"
-              key="address"
-            />
-            <Column
-              title="Tags"
-              dataIndex="tags"
-              key="tags"
-              render={tags => (
-                <span>
-                  {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
-                </span>
-              )}
-            />
-            <Column
-              title="Action"
+              width="50px"
+              title=""
               key="action"
-              render={(text, record) => (
-                <span>
-                  <a href="javascript:;">Invite {record.lastName}</a>
-                  <Divider type="vertical" />
-                  <a href="javascript:;">Delete</a>
-                </span>
-              )}
+              render={ record => { 
+                const menu = (
+                  <Menu>
+                    <Menu.Item>
+                      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">Editar</a>
+                    </Menu.Item>
+                    <Menu.Item>
+                    {
+                      record.estado.name === 'ACTIVO' ? 
+                      ( <a target="_blank" rel="noopener noreferrer" onClick={this.desactivar.bind(this, record)}> Desactivar</a> ) :
+                      ( <a target="_blank" rel="noopener noreferrer" onClick={this.activar.bind(this, record)}> Activar</a> )
+                    }
+                    </Menu.Item>
+                  </Menu>
+                );
+                return (
+                  <Dropdown overlay={menu}>
+                  <a className="ant-dropdown-link" href="#">
+                    <Icon type="setting" theme="outlined" />
+                  </a>
+                </Dropdown>
+              )}}
             />
           </Table>
         )
--- src/views/oficinas/index.js
+++ src/views/oficinas/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Col, Layout, Button } from 'antd';
+import { Col, Layout, Button, Menu, Dropdown, Icon, Modal, Upload} from 'antd';
 import { TheContent, TheHeader } from '../../components/layout';
 import OficinasList from './OficinasList';
 import OficinasForm from './OficinasForm';
+import axios from 'axios';
+import API from '../../Services/Api';
 
 export default class Oficinas extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      modalVisible: false
+      archivo: '',
+      modalVisible: false,
+      cargaVisible: false,
+      fileList: []
     }
   }
 
@@ -17,8 +22,24 @@ export default class Oficinas extends React.Component {
     this.setState({ modalVisible: true });
   }
 
+  showModalCarga = () => {
+    this.setState({ cargaVisible: true });
+  }
+
   handleCancel = () => {
     this.setState({ modalVisible: false });
+  }
+
+  hideCarga = () => {
+    this.setState({ cargaVisible: false });
+  }
+
+  subir = () => {
+    let file =  this.state.fileList[0];
+    console.log(file);
+    let formData = new FormData();
+    formData.append('file', file);
+    API.post('oficinas/carga', formData);
   }
 
   handleCreate = () => {
@@ -39,19 +60,57 @@ export default class Oficinas extends React.Component {
   }
 
   render() {
+
+    const menu = (
+      <Menu>
+        <Menu.Item onClick={this.showModal} key="1">Nueva Oficina</Menu.Item>
+        <Menu.Item onClick={this.showModalCarga} key="2">Cargar datos</Menu.Item>
+      </Menu>
+    );
+
+    const props = {
+      onRemove: (fileForm) => {
+        this.setState({...this.state, fileList: []});
+      },
+      beforeUpload: (fileForm) => {
+        this.setState({...this.state, fileList: [fileForm]});
+        return false;
+      },
+      fileList: this.state.fileList
+    };
+
     return (
         <Layout>
           <TheHeader>
             <Col span={12}>
-              <h2> Oficinas </h2>
+              <h1> Oficinas </h1>
             </Col>
             <Col span={12} align="right">
-              <Button type="primary" onClick={this.showModal}> Nuevo </Button>
+            <Dropdown overlay={menu}>
+              <Button type="primary">
+                Acciones <Icon type="down" />
+              </Button>
+            </Dropdown>
             </Col>
           </TheHeader>
           <TheContent>
               <OficinasList/>
               <OficinasForm visible={this.state.modalVisible} onCancel={this.handleCancel} onCreate={this.handleCreate} wrappedComponentRef={this.saveFormRef}/>
+              <Modal
+                title="Cargar oficinas"
+                visible={this.state.cargaVisible}
+                onOk={this.subir}
+                onCancel={this.hideCarga}
+                okText="Subir"
+                cancelText="Cancelar"
+              >
+                 <Upload {...props}>
+                  <Button>
+                    Seleccionar archivo
+                  </Button>
+                </Upload>
+              </Modal>
+      
           </TheContent>
         </Layout>
     )
