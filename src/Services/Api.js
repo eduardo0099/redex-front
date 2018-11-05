ff663b3ae2573f690cae9c86c4853fe18c76eb32
Johann Morales <johann.morales@pucp.edu.pe>
2018-11-04 21:57:53 -0500

    update

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -3,7 +3,6 @@ import backend from './backend';
 
 const API =  axios.create({
     baseURL: backend,
-    timeout: 10000,
     crossDomain: true,
     headers: {
         'Content-Type': 'application/json',
