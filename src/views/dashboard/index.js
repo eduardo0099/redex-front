248a1c49c07c67336808d80dc4a5770b76fbaca9
Johana Gamboa <johana27094@hotmail.com>
2018-11-18 00:28:50 -0500

    avance dashboard, revisar

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,31 +1,77 @@
 import React, { Component } from "react";
-import { Layout, Row, Col } from "antd";
+import { Layout, Row, Col, Divider } from "antd";
 import { TheContent, TheHeader } from "../../components/layout";
 import CrimsonChartCard from "../../components/CrimsonChartCard";
 
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,AreaChart, Area,ComposedChart, Line} from 'recharts';
 
 const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Enero', paquetes: 400, amt: 2400},
+      {name: 'Febrero', paquetes: 300, amt: 2210},
+      {name: 'Marzo', paquetes: 200, amt: 2290},
+      {name: 'Abril', paquetes: 278, amt: 2000},
+      {name: 'Mayo', paquetes: 189, amt: 2181},
+      {name: 'Junio', paquetes: 239, amt: 2500},
+      {name: 'Julio', paquetes: 239, amt: 2500},
+      {name: 'Agosto', paquetes: 349, amt: 2100},
+      {name: 'Setiembre', paquetes: 349, amt: 2100},
+      {name: 'Octubre', paquetes: 10, amt: 2100},
+      {name: 'Noviembre', paquetes: 349, amt: 2100},
+      {name: 'Diciembre', paquetes: 0, amt: 2100},
+];
+
+const vuelos=[
+  {name: 'Peru-Francia', paquetes: 400},
+  {name: 'Chile-Brasil', paquetes: 300},
+  {name: 'Francia-Lima', paquetes: 250},
+  {name: 'Rusia-España', paquetes: 400},
+  {name: 'Grecia-Italia', paquetes: 400}
+  
+];
+
+const oficinas=[
+  {name:'Peru',registrados:250,entregados:100},
+  {name:'Chile',registrados:150,entregados:50},
+  {name:'Brasil',registrados:350,entregados:100},
+  {name:'Bolivia',registrados:170,entregados:160},
+  {name:'Rusia',registrados:60,entregados:140},
+  {name:'España',registrados:123,entregados:90}
 ];
 const SimpleBarChart = (
-    	<BarChart width={600} height={300} data={data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+    	<BarChart width={450} height={190} data={data}
+      margin={{top:0, right: 0, left:-20, bottom: 0}}>
        <CartesianGrid strokeDasharray="3 3"/>
        <XAxis dataKey="name"/>
        <YAxis/>
        <Tooltip/>
        <Legend />
-       <Bar dataKey="pv" fill="#8884d8" />
-       <Bar dataKey="uv" fill="#82ca9d" />
+       <Bar dataKey="paquetes" fill="#82ca9d" />
       </BarChart>
     );
+
+const SimpleAreaChart = (
+  <AreaChart width={450} height={190} data={vuelos}
+            margin={{top: 0, right: 0, left: -20, bottom: 0}}>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <Tooltip/>
+        <Area type='monotone' dataKey='paquetes' stroke='#8884d8' fill='#8884d8' />
+      </AreaChart>
+);
+
+const LineaBarComposeChart = (
+  <ComposedChart width={600} height={190} data={oficinas}
+            margin={{top: 0, right: 0, bottom: 0, left: 200}}>
+          <CartesianGrid stroke='#f5f5f5'/>
+          <XAxis dataKey="name"/>
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey='registrados' barSize={20} fill='#413ea0' />
+          <Line type='monotone' dataKey='entregados' stroke='#ff7300' />
+       </ComposedChart>
+);
 
 
 class Dashboard extends Component {
@@ -43,16 +89,23 @@ class Dashboard extends Component {
         <TheContent>
           <Row>
             <Col span={12} style={colStyle}>
+              <Divider >paquetesXoficina</Divider>
               <CrimsonChartCard style={{...middleHeight}}>
               {SimpleBarChart}
               </CrimsonChartCard>
             </Col>
             <Col span={12} style={colStyle}>
-              <CrimsonChartCard style={{...middleHeight}} />
+              <Divider >vuelosXpaquetes(vuelos mas usuados?)</Divider>
+              <CrimsonChartCard style={{...middleHeight}} >
+              {SimpleAreaChart}
+              </CrimsonChartCard>
             </Col>
           </Row>
           <Row style={colStyle}> 
-            <CrimsonChartCard style={{...middleHeight}} />
+            <Divider >paquetesXTodasOficina(oficinas con mas transito, ultimos meses?)</Divider>
+            <CrimsonChartCard style={{...middleHeight}} >
+            {LineaBarComposeChart}
+            </CrimsonChartCard>
           </Row>
         </TheContent>
       </Layout>
