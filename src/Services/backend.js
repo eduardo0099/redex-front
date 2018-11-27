32c1a6608602e1139fa5c8bed1874b41b5957b21
Johann Morales <johann.morales@pucp.edu.pe>
2018-11-27 15:15:54 -0500

    update

--- src/Services/backend.js
+++ src/Services/backend.js
@@ -1,8 +1,6 @@
 // cambiar a false o true si quieren usar heroku o no
 const use_heroku = false ;
 
-
-
 const url = use_heroku ? 'https://redex-backend.herokuapp.com/' : 'http://localhost:5000/';
 
 export default url;
