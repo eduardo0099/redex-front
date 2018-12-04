67c852c958d7884fc9e5a2be5ea4f3c612dd5206
Oscar <Oscar@DESKTOP-44C66HF>
2018-12-04 13:33:26 -0500

    s2

--- src/Services/backend.js
+++ src/Services/backend.js
@@ -1,12 +1,9 @@
 // cambiar a false o true si quieren usar heroku o no
 const use_heroku = false ;
 
-<<<<<<< HEAD
-const url = use_heroku ? 'https://redex-backend.herokuapp.com/' : 'http://localhost:5000/';
-=======
 
-const url = use_heroku ? 'https://redex-backend.herokuapp.com/' : 'http://10.101.57.155:5000/';
->>>>>>> 2789fd34845900ae27dc6c72e48731184c961350
+const url = use_heroku ? 'https://redex-backend.herokuapp.com/' : 'http://localhost:5000/';
+
 
 export default url;
 
